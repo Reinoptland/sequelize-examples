d792216ed89252403991915037fac8ea4cf38e43
Reinoptland <reinoptland@gmail.com>
2020-07-14 16:23:41 +0200

    Make a route for GET /users

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express();
 
 const PORT = 4000;
 
+app.get("/users", (req, res) => {
+  console.log("HI! IS THIS WORKING?");
+  res.send("TESTING");
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
