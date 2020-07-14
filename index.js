5e8bac939949cda53a9bbde865db0ed97557654b
Reinoptland <reinoptland@gmail.com>
2020-07-14 16:31:05 +0200

    Add error handling

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const app = express();
 const PORT = 4000;
 
 app.get("/users", async (req, res) => {
-  console.log("HI! IS THIS WORKING?");
-  const users = await User.findAll();
+  //   console.log("HI! IS THIS WORKING?");
+  try {
+    const users = await User.findAll();
+    // console.log("ALL USERS?", users);
 
-  console.log("ALL USERS?", users);
-
-  res.json(users);
+    res.json(users);
+  } catch (error) {
+    res.status(400).send({ message: "Error in getting users, sorry" });
+  }
 });
 
 app.listen(PORT, () => {
